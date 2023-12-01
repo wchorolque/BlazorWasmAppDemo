33985011a4234b18a3f32a98db3388632bd23e43
Williams I. Chorolque Choque <wchorolque@gmail.com>
2023-11-30 22:32:18 -0400

    Configurado conexion a api de platzi

--- Program.cs
+++ Program.cs
@@ -6,6 +6,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var apiSettingsUrl = builder.Configuration.GetValue<string>("apiUrl");
+builder.Services.AddScoped(sp => 
+new HttpClient {
+    BaseAddress = new Uri(apiSettingsUrl)
+});
 
 await builder.Build().RunAsync();
